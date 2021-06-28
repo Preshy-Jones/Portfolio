fe9b6d2fc4573ecde1fb271521881206e8461dca
Preshy-Jones <adedibuprecious@gmail.com>
2021-06-28 17:19:01 +0100

    updated info

--- src/profile.js
+++ src/profile.js
@@ -9,7 +9,7 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Precious Adedibu",
 };
 const background = {
   // Options: Snow or Particle
@@ -20,7 +20,7 @@ const background = {
 const section2title = "About Me";
 const about = {
   paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+    "A Software Developer who loves to transform ideas into reality using code. I am skilled in creating responsive web applications with Html, css and front end frameworks such as React. I am also skilled in creating high performance apis back end technologies such as with nodejs and laravel and Databases such as Mysql and Mongo DB. My passion for programming drives me to get better at better at delivering quality services ",
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
@@ -38,6 +38,10 @@ const skillsBar = [
     faClass: "fab fa-js",
   },
   {
+    name: 'React',
+    faClass: 'fab fa-react'
+  },
+  {
     name: "SASS",
     faClass: "fab fa-sass",
   },
@@ -50,17 +54,22 @@ const skillsBar = [
     faClass: "fab fa-python",
   },
   {
-    name: "Java",
-    faClass: "fab fa-java",
-  },
-  {
     name: "PHP",
     faClass: "fab fa-php",
+  },
+  {
+    name: 'Laravel',
+    faClass: 'fab fa-laravel'
   },
   {
     name: "Database",
     faClass: "fas fa-database",
   },
+  {
+    name: "Github",
+    faClass: "fab fa-github"
+  },
+
   {
     name: "AWS",
     faClass: "fab fa-aws",
@@ -69,73 +78,73 @@ const skillsBar = [
 // Edit your projects, its name, your skills used to make it, and the url.
 // You can omit freely anything if you dont have it
 // To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
-const section3Title = "Past Projects";
+const section3Title = "Front End Projects";
 const projects = [
   {
     // Add image in './styles/images.css' in #project1
     id: "project1",
-    name: "Project 1",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "Burjtech.org",
+    skills: ["HTML, CSS, JS, Node JS"],
+    url: "http://burjtech.org",
   },
   {
     // Add image in './styles/images.css' in #project2
     id: "project2",
-    name: "Project 2",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "Sis Certifications Nigeria",
+    skills: ["HTML, CSS, JS, React, Node JS"],
+    url: "https://siscertificationsnigeria.herokuapp.com",
   },
   {
     // Add image in './styles/images.css' in #project3
     id: "project3",
-    name: "Project 3",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "Art Scope",
+    skills: ["HTML, CSS"],
+    url: "https://preshy-jones.github.io/Art-Scope/",
   },
   {
     // Add image in './styles/images.css' in #project4
     id: "project4",
-    name: "Project 4",
+    name: "Dog Website",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    url: "https://preshy-jones.github.io/DOG/",
   },
   {
     // Add image in './styles/images.css' in #project5
     id: "project5",
-    name: "Project 5",
+    name: "Anime Gallery Touch slider",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    url: "https://animegallery.herokuapp.com/",
   },
-  {
-    // Add image in './styles/images.css' in #project6
-    id: "project6",
-    name: "Project 6",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
+  // {
+  //   // Add image in './styles/images.css' in #project6
+  //   id: "project6",
+  //   name: "Project 6",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
 ];
 // Edit your Miscellaneous Activities, its name and the url.
 // You can omit freely anything if you dont have it
 // To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
-const section4Title = "Miscellaneous";
+const section4Title = "Backend Projects";
 const miscellaneous = [
   {
     // Add image in './styles/images.css' in #misc1
     id: "misc1",
-    name: "Miscellaneous 1",
-    url: "https://github.com/kaustubhai",
+    name: "Book Store",
+    url: "https://preshybooks.herokuapp.com/",
   },
   {
     // Add image in './styles/images.css' in #misc2
-    id: "misc2",
-    name: "Miscellaneous 2",
-    url: "https://github.com/kaustubhai",
+    id: "Nodejs",
+    name: "Nodejs Blog",
+    url: "https://preshyblog.herokuapp.com/",
   },
   {
     // Add image in './styles/images.css' in #misc3
     id: "misc3",
-    name: "Miscellaneous 3",
-    url: "https://github.com/kaustubhai",
+    name: "URL Shortener",
+    url: "https://preshyurlshortener.herokuapp.com/shorten",
   },
 ];
 // Contact form text, and Jotforms link
@@ -144,18 +153,18 @@ const miscellaneous = [
 const section5Title = "Get in Touch";
 const contact = {
   pitch:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
+    "Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you. Simply fill the from and send me an email.",
   copyright: "Kaustubh Mishra",
   contactUrl: "",
 };
 // Paste your respective social media links. You can omit any if you dont have it
 // Upload your resume in your drive, get the shaareable link and paste it in the resume section
 const social = {
-  github: "https://github.com",
+  github: "https://github.com/Preshy-Jones",
   facebook: "https://facebook.com",
-  linkedin: "https://linkedin.com",
+  linkedin: "https://www.linkedin.com/in/precious-adedibu-568aa211a/",
   instagram: "https://instagram.com",
-  resume: "https://novoresume.com/",
+  resume: "https://drive.google.com/drive/u/0/search?q=resume",
 };
 // Dont change anything here
 export {
