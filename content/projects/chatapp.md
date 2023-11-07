0465a705c4c15cc9b80c6b9a2d0dcb40f1abd6ea
Precious Adedibu <adedibuprecious@gmail.com>
2023-11-08 00:01:53 +0100

    update

---
+++ content/projects/chatapp.md
@@ -0,0 +1,10 @@
+---
+date: '2022-12-01'
+title: 'Chatapp'
+github: 'https://github.com/Preshy-Jones/Websockets---Chat-app---Backend'
+tech:
+  - Nodejs
+  - Nestjs
+  - Socket.io
+showInProjects: true
+---
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -191,7 +191,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me2.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
